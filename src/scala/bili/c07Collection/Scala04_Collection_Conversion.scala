b23d2466a75afad110c98a1c32fefb26ffd3a047
Lisa <lilxysa0723p@gmail.com>
2023-06-26 18:11:11 +0800

    C7. Collections - 4. Conversion between collections

---
+++ src/scala/bili/c07Collection/Scala04_Collection_Conversion.scala
@@ -0,0 +1,39 @@
+package scala.bili.c07Collection
+
+/**
+ * @author Lisa
+ * @time 2023/6/26 - 12:48
+ * @description
+ */
+object Scala04_Collection_Conversion {
+  def main(args: Array[String]): Unit = {
+    //TODO 集合的转化
+
+    val list = List(1,2,3,4)
+    val list_kv = List(("a",1))
+
+    val array: Array[Int] = list.toArray
+    val set: Set[Int] = list.toSet
+    //list.toMap//list没有键值对概念，无法变Map
+    list_kv.toMap
+
+    println(list)
+    println(array)
+    println(set)
+
+
+
+    val set2 = Set(1,2,3,4)
+
+    val array2: Array[Int] = set.toArray
+    val list2: List[Int] = set.toList
+    //set2.toMap
+
+
+    val map = Map(("a", 1), ("b", 2))
+
+    val array3: Array[(String, Int)] = map.toArray
+    val list3: List[(String, Int)] = map.toList
+    val sety3: Set[(String, Int)] = map.toSet
+  }
+}
